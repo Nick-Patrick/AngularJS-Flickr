0eb3c55f115e6a10149065f222fdc369040b25f0
nickpatrick <n.patrick@outlook.com>
2015-07-21 19:18:54 +0100

    Cleanup redundant files.

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -5,11 +5,8 @@
 
     beforeEach(module('potato'));
 
-    it('should define more than 5 awesome things', inject(function($controller) {
-      var vm = $controller('MainController');
-
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length > 5).toBeTruthy();
-    }));
+    it('test', function () {
+      expect('a').toEqual('a');
+    });
   });
 })();
